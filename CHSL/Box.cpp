9e6aee4f3c80f29ac530d8ae5223c7696d1a6805
crazysissen <crazysissen@gmail.com>
2021-11-11 15:13:44 +0100

    Box collision.

--- CHSL/Box.cpp
+++ CHSL/Box.cpp
@@ -1,6 +1,8 @@
 #include "core.h"
 #include "Box.h"
 
+#include <limits>
+
 cs::Box::Box(Vec3 center, Vec3 xV, Vec3 yV, Vec3 zV)
 	:
 	m_center(center),
@@ -10,7 +12,7 @@ cs::Box::Box(Vec3 center, Vec3 xV, Vec3 yV, Vec3 zV)
 {
 }
 
-cs::Box::Box(Vec3 center, float width, float height, float depth, float rX, float rY, float rZ)
+cs::Box::Box(Vec3 center, float width, float height, float depth)
 	:
 	m_center(center),
 	m_x(1, 0, 0),
@@ -20,6 +22,12 @@ cs::Box::Box(Vec3 center, float width, float height, float depth, float rX, floa
 	m_h(height),
 	m_d(depth)
 {
+}
+
+cs::Box::Box(Vec3 center, float width, float height, float depth, float rX, float rY, float rZ)
+	:
+	Box(center, width, height, depth)
+{
 	Mat3 r = Mat::rotation3(rX, rY, rZ);
 	m_x = r * m_x;
 	m_y = r * m_y;
@@ -28,31 +36,60 @@ cs::Box::Box(Vec3 center, float width, float height, float depth, float rX, floa
 
 bool cs::Box::Raycast(const Line3& line, float& out) const
 {
-	const Vec3* d[] = { &m_x, &m_y, &m_z };
-	const float* f[] = { &m_w, &m_h, &m_d };
+	constexpr float epsilon = 1.0f / 65536;
 
-	// Parallel check
+	const Vec3* a[] = { &m_x, &m_y, &m_z }; // Side directions
+	const float h[] = { m_w, m_h, m_d }; // Halfsides
 
-	for (int i = 0; i < 3; i++)
+	// Collision check
+
+	float tMin = FLT_MIN;
+	float tMax = FLT_MAX;
+
+	Vec3 center = m_center - line.GetOrigin();
+
+	for (int i = 0; i < 3; ++i)
 	{
-		if (d[i]->Parallel3(line.GetDirection()))
+		float e = a[i]->Dot3(center);
+		float f = a[i]->Dot3(line.GetDirection());
+
+		if (fabs(f) > epsilon)
 		{
-			for (int j = 1; j < 3; j++)
+			float fInv = 1.0f / f;
+			bool swap = (h[i] > 0) == (fInv > 0);
+
+			float t1 = 
+				!swap * (e + h[i]) * fInv +
+				swap *	(e - h[i]) * fInv;
+
+			float t2 =
+				swap *	(e + h[i]) * fInv +
+				!swap * (e - h[i]) * fInv;
+
+			tMin =
+				(t1 > tMin) *	t1 +
+				!(t1 > tMin) *	tMin;
+
+			tMax =
+				(t1 < tMin) *	t1 +
+				!(t1 < tMin) *	tMin;
+
+			if (tMin > tMax || tMax < 0)
 			{
-				int index = imod(i + j, 3);
-				float parallelDist = d[index]->Dot3(m_center - line.GetOrigin());
-
-				if (parallelDist > *f[index])
-				{
-					return false;
-				}
+				return false;
 			}
-
-			return true;
+		}
+		else if (-e - h[i] > 0 || -e + h[i] < 0) // If parallel
+		{
+			return false;
 		}
 	}
 
+	out =
+		(tMin > 0) *	tMin +
+		!(tMin > 0) *	tMax;
 
+	return true;
 }
 
 bool cs::Box::Intersection(const Line3& line, Vec3& out) const
--- library/CHSL/include/CHSL/Box.h
+++ library/CHSL/include/CHSL/Box.h
@@ -9,7 +9,8 @@ namespace cs
 	{
 	public:
 		Box(Vec3 center, Vec3 xV, Vec3 yV, Vec3 zV);
-		Box(Vec3 center, float width, float height, float depth, float rX = 0, float rY = 0, float rZ = 0);
+		Box(Vec3 center, float width, float height, float depth);
+		Box(Vec3 center, float width, float height, float depth, float rX, float rY, float rZ);
 		
 		bool Raycast(const Line3& line, float& out) const override;
 		bool Intersection(const Line3& line, Vec3& out) const;
